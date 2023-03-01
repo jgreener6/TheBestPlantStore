2a95086af3f9656f5d2e404c320388a72ec95102
Grif Wakeman <grif@syncbeast.co>
2023-03-01 12:43:11 -0500

    Made the nav bar interactive with active headers. Also added cursor: pointer; to the product images.

--- LandingPage.js
+++ LandingPage.js
@@ -1,30 +1,41 @@
-(function (){
-const containers = document.querySelectorAll(".imageContainer");
-const images = document.querySelectorAll(".product-image");
-const info = document.querySelectorAll(".product-info");
-function imageClick (e){
-    
-    info.forEach(i => i.style.display="none");
-    containers.forEach(c => {
-        c.style.height = "unset";
-        c.style.backgroundColor = "unset";
-    });
+(function () {
+
+    const header = document.getElementById("nav-bar");
+    const btns = header.getElementsByClassName("filterBtn");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            const current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+
+    const containers = document.querySelectorAll(".imageContainer");
+    const images = document.querySelectorAll(".product-image");
+    const info = document.querySelectorAll(".product-info");
+    function imageClick(e) {
+
+        info.forEach(i => i.style.display = "none");
+        containers.forEach(c => {
+            c.style.height = "unset";
+            c.style.backgroundColor = "unset";
+        });
 
 
-   let image = e.target;
-   let container = image.parentElement;
-   container.style.height = "500px";
-   container.style.backgroundColor = "#aaaaaa";
-   
-   const pInfo = container.querySelector(".product-info");
-   pInfo.style.display = "block";
+        let image = e.target;
+        let container = image.parentElement;
+        container.style.height = "500px";
+        container.style.backgroundColor = "#aaaaaa";
 
-   
-   
+        const pInfo = container.querySelector(".product-info");
+        pInfo.style.display = "block";
 
 
 
-}
-images.forEach ((i)=>i.addEventListener("click", imageClick))
+
+
+
+    }
+    images.forEach((i) => i.addEventListener("click", imageClick))
 
 }());
